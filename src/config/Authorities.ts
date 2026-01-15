1720f65a859663dbe86133cbbc0e614afa7a8b20
David-Tarlos <david.tarlos@edu.tbz.ch>
2026-01-15 17:26:37 +0100

    add image post authorities to enum

--- src/config/Authorities.ts
+++ src/config/Authorities.ts
@@ -1,10 +1,13 @@
 enum authorities {
-/*
-  add your authorities here. E.g:
-*/
+  // User authorities
   USER_CREATE = "USER_CREATE",
   USER_READ = "USER_READ",
   USER_DEACTIVATE = "USER_DEACTIVATE",
+  // Post authorities
+  IMAGE_CREATE = "IMAGE_CREATE",
+  IMAGE_MODIFY = "IMAGE_MODIFY",
+  IMAGE_DELETE = "IMAGE_DELETE",
+  IMAGE_LIKE = "IMAGE_LIKE"
 }
 
 export default authorities;
