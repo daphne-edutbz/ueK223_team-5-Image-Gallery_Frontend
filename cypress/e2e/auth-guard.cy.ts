d24a4067b6751076a8c121d5a5aca2b8b89034da
Gino Wenger <gino.wenger@edu.tbz.ch>
2026-01-21 14:40:35 +0100

    Revert "Add Cypress auth guard test"
    This reverts commit ff3f1794bde3e54a7615ab4409351068b1c8da96.

--- cypress/e2e/auth-guard.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Auth guard', () => {
-  it('redirects to login when no token is present', () => {
-    cy.visit('/gallery');
-    cy.location('pathname').should('eq', '/login');
-  });
-});
