0616187770588a03378c20f5f4a97dabaa245d6f
Daphne McNamara <daphne.mcnamara@edu.tbz.ch>
2026-01-22 14:15:40 +0100

    Revert "format file"
    This reverts commit 42a21df687c4e9b5b6a81cb4ee916ff0a185a162.

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,35 +1,37 @@
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "../components/pages/LoginPage/LoginPage";
-import PrivateRoute from "./PrivateRoute";
-import HomePage from "../components/pages/HomePage";
-import UserTable from "../components/pages/UserPage/UserTable";
-import UserPage from "../components/pages/UserPage/UserPage";
-import authorities from "../config/Authorities";
+import {Route, Routes} from 'react-router-dom';
+import LoginPage from '../components/pages/LoginPage/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import HomePage from '../components/pages/HomePage';
+import UserTable from '../components/pages/UserPage/UserTable';
+import UserPage from '../components/pages/UserPage/UserPage';
+import authorities from '../config/Authorities';
 // @ts-ignore
-import GalleryPage from "../components/pages/GalleryPage/GalleryPage";
+import GalleryPage from '../components/pages/GalleryPage/GalleryPage';
 // @ts-ignore
-import MyPosts from "../components/pages/MyPostsPage/MyPosts";
+import MyPosts from '../components/pages/MyPostsPage/MyPosts'
 // @ts-ignore
-import AdminPosts from "../components/pages/AdminPostPage/AdminPostPage";
+import AdminPosts from '../components/pages/AdminPostPage/AdminPostPage'
+
 
 /**
  * Router component renders a route switch with all available pages
  */
 
 const Router = () => {
+
   /** navigate to different "home"-locations depending on Role the user have */
 
   return (
     <Routes>
-      <Route path={"/"} element={<HomePage />} />
-      <Route path={"/login"} element={<LoginPage />} />
+      <Route path={'/'} element={<HomePage />} />
+      <Route path={'/login'} element={<LoginPage />} />
 
       <Route
-        path={"/user"}
+        path={'/user'}
         element={<PrivateRoute requiredAuths={[]} element={<UserTable />} />}
       />
       <Route
-        path="/user/edit"
+        path='/user/edit'
         element={
           <PrivateRoute
             requiredAuths={[authorities.USER_CREATE]}
@@ -38,7 +40,7 @@ const Router = () => {
         }
       />
       <Route
-        path="/user/edit/:userId"
+        path='/user/edit/:userId'
         element={
           <PrivateRoute
             requiredAuths={[authorities.USER_CREATE]}
@@ -46,32 +48,35 @@ const Router = () => {
           ></PrivateRoute>
         }
       />
-      <Route
-        path="/gallery"
+        <Route
+        path='/gallery'
         element={
-          <PrivateRoute
-            requiredAuths={[]}
-            element={<GalleryPage />}
-          ></PrivateRoute>
+            <PrivateRoute
+                requiredAuths={[]}
+                element={<GalleryPage/>}
+                    ></PrivateRoute>
         }
-      />
-      <Route
-        path="/gallery/my-posts"
-        element={
-          <PrivateRoute requiredAuths={[]} element={<MyPosts />}></PrivateRoute>
-        }
-      />
-      <Route
-        path="/admin/posts"
-        element={
-          <PrivateRoute
-            requiredAuths={[authorities.IMAGE_MODIFY]}
-            element={<AdminPosts />}
-          />
-        }
-      />
+        />
+        <Route
+            path='/gallery/my-posts'
+            element={
+                <PrivateRoute
+                    requiredAuths={[]}
+                    element={<MyPosts/>}
+                ></PrivateRoute>
+            }
+        />
+        <Route
+            path='/admin/posts'
+            element={
+                <PrivateRoute
+                    requiredAuths={[authorities.IMAGE_MODIFY]}
+                    element={<AdminPosts/>}
+                />
+            }
+        />
 
-      <Route path="*" element={<div>Not Found</div>} />
+      <Route path='*' element={<div>Not Found</div>} />
     </Routes>
   );
 };
